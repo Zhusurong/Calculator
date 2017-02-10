1e71355a10a91060c4956d637f014b2e48a19063
Zhusurong <2037288572@qq.com>
2017-02-10 14:12:09 +0800

    Initial commit

---
+++ app/src/main/java/com/example/calculator/GoogleApiClient.java
@@ -0,0 +1,7 @@
+package com.example.calculator;
+
+/**
+ * Created by 20372 on 2017/2/10.
+ */
+public class GoogleApiClient {
+}
---
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -0,0 +1,157 @@
+package com.example.calculator;
+
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button btn_0;
+    Button btn_1;
+    Button btn_2;
+    Button btn_3;
+    Button btn_4;
+    Button btn_5;
+    Button btn_6;
+    Button btn_7;
+    Button btn_8;
+    Button btn_9;
+    Button btn_point;
+    Button btn_clear;
+    Button btn_del;
+    Button btn_pluse;
+    Button btn_minus;
+    Button btn_multiply;
+    Button btn_divide;
+    Button btn_equle;
+    EditText et_input;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+    boolean clear_flag;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Object findViewById;
+        btn_0=(Button) findViewById(R.id.btn_0);
+        btn_1=(Button) findViewById(R.id.btn_1);
+        btn_2=(Button) findViewById(R.id.btn_2);
+        btn_3=(Button) findViewById(R.id.btn_3);
+        btn_4=(Button) findViewById(R.id.btn_4);
+        btn_5=(Button) findViewById(R.id.btn_5);
+        btn_6=(Button) findViewById(R.id.btn_6);
+        btn_7=(Button) findViewById(R.id.btn_7);
+        btn_8=(Button) findViewById(R.id.btn_8);
+        btn_9=(Button) findViewById(R.id.btn_9);
+        btn_point=(Button)findViewById(R.id.btn_point);
+        btn_clear=(Button)findViewById(R.id.btn_clear);
+        btn_del=(Button) findViewById(R.id.btn_del);
+        btn_pluse=(Button) findViewById(R.id.btn_pluse);
+        btn_minus=(Button) findViewById(R.id.btn_minus);
+        btn_multiply=(Button) findViewById(R.id.btn_multiply);
+        btn_divide=(Button) findViewById(R.id.btn_divide);
+        btn_equle=(Button) findViewById(R.id.btn_equal);
+        et_input=(EditText) findViewById(R.id.et_input);
+
+        btn_0.setOnClickListener(this);
+        btn_1.setOnClickListener(this);
+        btn_2.setOnClickListener(this);
+        btn_3.setOnClickListener(this);
+        btn_4.setOnClickListener(this);
+        btn_5.setOnClickListener(this);
+        btn_6.setOnClickListener(this);
+        btn_7.setOnClickListener(this);
+        btn_8.setOnClickListener(this);
+        btn_9.setOnClickListener(this);
+        btn_point.setOnClickListener(this);
+        btn_clear.setOnClickListener(this);
+        btn_del.setOnClickListener(this);
+        btn_pluse.setOnClickListener(this);
+        btn_minus.setOnClickListener(this);
+        btn_multiply.setOnClickListener(this);
+        btn_divide.setOnClickListener(this);
+        btn_equle.setOnClickListener(this);
+
+
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+    @Override
+    public void onClick(View v) {
+        String str = et_input.getText().toString();
+        switch (v.getId()){
+            case R.id.btn_0:
+            case R.id.btn_1:
+            case R.id.btn_2:
+            case R.id.btn_3:
+            case R.id.btn_4:
+            case R.id.btn_5:
+            case R.id.btn_6:
+            case R.id.btn_7:
+            case R.id.btn_8:
+            case R.id.btn_9:
+            case R.id.btn_point:
+                if(clear_flag){
+                    clear_flag = false;
+                    str="";
+                    et_input.setText("");
+                }
+                et_input.setText(str+((Button)v).getText());
+                break;
+            
+
+        }
+
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Main Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
+    }
+}
